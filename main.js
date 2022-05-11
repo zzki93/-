d35515df22d88cfd2d39f6ab2d74ae44688cdb43
zzki93 <rlaqudrms12@gmail.com>
2022-05-11 14:46:54 +0900

    글 생성 수정 삭제 기능 추가

--- main.js
+++ main.js
@@ -12,14 +12,13 @@ function getList(filelist){
       <a href="/?id=${filelist[i]}">${filelist[i]}</a>
     </li>
     `
-    
     i++;
   }
   pageList = pageList + '</ul>';
   return pageList;
 }
 
-function getTemplateHtml(title,list,body){
+function getTemplateHtml(title,list,body, control){
   return  `
   <!doctype html>
   <html>
@@ -30,13 +29,21 @@ function getTemplateHtml(title,list,body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">create</a>
-    <h2>${title}</h2>
-    <p>${body}</p>
+    ${control}
+    ${body}
   </body>
   </html>
 `
 }
+function control(title){ return `
+    <a href="/create">create</a>
+    <a href="/update?id=${title}">update</a>
+    <form action="delete_process" method="post">
+      <input type='hidden' name='title' value='${title}'>
+      <input type='submit' value='delete'>
+    </form>
+    `}
+
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -49,9 +56,12 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data',(err,filelist) =>{
           
           let title = 'Welcome'
+          let data = 'hello, node.js';
           let list = getList(filelist);
-          data = 'hello, node.js';
-          let template = getTemplateHtml(title,list,data);
+          let template = getTemplateHtml(title,list,
+          `<h2>${title}</h2>
+          <p>${data}</p>`
+          ,`<a href="/create">create</a>`);
           response.writeHead(200);
           response.end(template);
         })
@@ -62,7 +72,9 @@ var app = http.createServer(function(request,response){
       fs.readdir('./data',(err,filelist) =>{
         let title = queryData.get('id');
         let list = getList(filelist);
-        let template = getTemplateHtml(title,list,data);
+        let template = getTemplateHtml(title,list,
+        `<h2>${title}</h2>
+        <p>${data}</p>`,control(title));
         response.writeHead(200);
         response.end(template);
       })
@@ -78,10 +90,10 @@ var app = http.createServer(function(request,response){
             <textarea name="description" id="" cols="30" rows="10" placeholder="description"></textarea>
           </p>
           <p>
-            <input type="submit">
+            <input type="submit" value="생성">
           </p>
         </form>
-      `);
+      `,'');
       response.writeHead(200);
       response.end(template);
     })
@@ -93,12 +105,82 @@ var app = http.createServer(function(request,response){
     })
     request.on('end',()=>{
       let post = new URLSearchParams(body);
-      fs.writeFile(`data/${post.get('title')}`, post.get('description'), function (err) {
-        if (err) throw err;
-        response.end(alert('저장되었습니다.'));
+      console.log(post)
+      let title = post.get('title')
+      let description = post.get('description')
+      fs.writeFile(`./data/${title}`, description,"utf8", function (err) {
+        console.log('File is created successfully.');
+        response.writeHead(302,{
+          'Location': encodeURI(`/?id=${title}`)
+        })
+        response.end()
       });
+     
+    });
 
+    
+  } else if(pathname === '/update'){
+    fs.readFile(`data/${queryData.get('id')}`,'utf8',(err,des) =>{
+      fs.readdir('./data',(err,filelist) =>{
+        let title = queryData.get('id');
+        let list = getList(filelist);
+        let template = getTemplateHtml(title,list,'',`
+          <form action="http://localhost:1234/update_process" method="post">
+          <input type='hidden' name='id' value="${title}">
+          <p><input type='text' name="title" value="${title}"></p>
+          <p>
+            <textarea name="description" id="" cols="30" rows="10" placeholder="description" >${des}</textarea>
+          </p>
+          <p>
+            <input type="submit" value="수정">
+          </p>
+          </form>
+        `);
+        response.writeHead(200);
+        response.end(template);
+      })
     })
+  } else if(pathname === '/update_process'){
+    response.writeHead(200);
+    let body = '';
+    request.on('data',(data)=>{
+      body += data;
+    })
+    request.on('end',()=>{
+      let post = new URLSearchParams(body);
+      console.log(post)
+      let id = post.get('id')
+      let title = post.get('title')
+      let description = post.get('description')
+      fs.writeFile(`./data/${id}`, description,"utf8", function (err) {
+        fs.rename(`./data/${id}`,`./data/${title}`,(err)=>{
+          if(err){return err}
+          else {console.log('file renamed')}
+        })
+        response.writeHead(302,{
+          'Location': encodeURI(`/?id=${title}`)
+        })
+        response.end()
+      });
+    });
+  } else if(pathname === '/delete_process'){
+    response.writeHead(200);
+    let body = ''
+    request.on('data',(data)=>{
+      body += data
+    })
+    request.on('end',()=>{
+      let post = new URLSearchParams(body)
+      let title = post.get('title')
+      fs.unlink(`data/${title}`,(err)=>{
+        if(err) return err
+        response.writeHead(302,{
+          'Location': '/'
+        })
+        response.end()
+      })
+    })
+
   } else{
     response.writeHead(404);
     response.end('not found');
@@ -106,7 +188,7 @@ var app = http.createServer(function(request,response){
     
     // console.log(__dirname + url)
     
-    // response.end('zzki : ' + url);
+    // response.end('zzki : ' + url);C:\Users\WR\Desktop\web\lifecode\node
 
  
 });
